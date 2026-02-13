f0aabddb65017cd423b7fa0b9230031125d9ec58
jose-gomes1 <145759337+jose-gomes1@users.noreply.github.com>
2026-02-13 16:16:23 -0100

    Update storage.py

--- storage.py
+++ storage.py
@@ -2,13 +2,15 @@ import sqlite3
 from pathlib import Path
 import os
 
-IS_STREAMLIT = os.getenv("STREAMLIT_SERVER_RUNNING") == "1"
+# If /mount/data exists, use it (Streamlit Cloud)
+if Path("/mount/data").exists():
+    DB_DIR = Path("/mount/data")
+else:
+    DB_DIR = Path(__file__).parent
 
-DB_DIR = Path("/mount/data") if IS_STREAMLIT else Path(__file__).parent
 DB_DIR.mkdir(parents=True, exist_ok=True)
 
 DB_PATH = DB_DIR / "frota.db"
-
 
 def get_conn():
     conn = sqlite3.connect(DB_PATH, check_same_thread=False)
